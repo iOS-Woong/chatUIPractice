fea2a7e9c31d3346a7543d130b5ad0efcd37e4a1
woong <workplayhard1@naver.com>
2023-07-30 03:00:55 +0900

    feat: messagesCollectionView의 세가지 프로토콜 채택 (MessagesDatasource, MessagesLayoutDelegate, MessagesDisplayDelegate)

--- MessageKitPractice/MessageKitPractice/ChatViewController.swift
+++ MessageKitPractice/MessageKitPractice/ChatViewController.swift
@@ -12,6 +12,37 @@ class ChatViewController: MessagesViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupMessageCollectionViewAttributes()
     }
+    
+    private func setupMessageCollectionViewAttributes() {
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+    
+}
+
+extension ChatViewController: MessagesDataSource {
+    var currentSender: MessageKit.SenderType {
+        <#code#>
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+        <#code#>
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        <#code#>
+    }
+    
+    
+}
+
+extension ChatViewController: MessagesLayoutDelegate {
+    
+}
+
+extension ChatViewController: MessagesDisplayDelegate {
+    
 }
