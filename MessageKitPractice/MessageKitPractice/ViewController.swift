889ac0727cd4150ca93645bed34762da832fbd07
woong <workplayhard1@naver.com>
2023-07-30 02:44:33 +0900

    feat: tableView 생성 및  Datasource Delegate 초기구현

--- MessageKitPractice/MessageKitPractice/ViewController.swift
+++ MessageKitPractice/MessageKitPractice/ViewController.swift
@@ -14,23 +14,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
+        setupTableViewAttributes()
+        setupViews()
     }
 
     private func setupNavigation() {
         title = "Chat List"
     }
+    
+    private func setupTableViewAttributes() {
+        chatListTableView.dataSource = self
+        chatListTableView.delegate = self
+    }
+    
+    private func setupViews() {
+        chatListTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(chatListTableView)
+        
+        NSLayoutConstraint.activate([
+            chatListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            chatListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            chatListTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            chatListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "John"
+        
+        return cell
     }
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
