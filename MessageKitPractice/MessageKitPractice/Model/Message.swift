5de03825e6f9a9b9ad14206bc12222ddc63dc2f9
woong <workplayhard1@naver.com>
2023-07-31 02:53:44 +0900

    feat: Message 이니셜라이저 방법 분리

--- MessageKitPractice/MessageKitPractice/Model/Message.swift
+++ MessageKitPractice/MessageKitPractice/Model/Message.swift
@@ -5,8 +5,8 @@
 //  Created by 서현웅 on 2023/07/30.
 //
 
-import Foundation
 import MessageKit
+import UIKit
 
 struct Sender: SenderType {
     var senderId: String
@@ -18,4 +18,33 @@ struct Message: MessageType {
     var messageId: String
     var sentDate: Date
     var kind: MessageKind
+    
+    private init(sender: SenderType, messageId: String, date: Date, kind: MessageKind) {
+        self.sender = sender
+        self.kind = kind
+        self.messageId = messageId
+        self.sentDate = date
+    }
+    
+    init(text: String, sender: SenderType, messageId: String, date: Date) {
+        self.init(sender: sender, messageId: messageId, date: date, kind: .text(text))
+    }
+    
+    init(image: UIImage, sender: SenderType, messageId: String, date: Date) {
+        let mediaItem = ImageMediaItem(image: image)
+        self.init(sender: sender, messageId: messageId, date: date, kind: .photo(mediaItem))
+    }
+}
+
+private struct ImageMediaItem: MediaItem {
+    var url: URL?
+    var image: UIImage?
+    var placeholderImage: UIImage
+    var size: CGSize
+    
+    init(image: UIImage) {
+        self.image = image
+        size = CGSize(width: 240, height: 240)
+        placeholderImage = UIImage()
+    }
 }
