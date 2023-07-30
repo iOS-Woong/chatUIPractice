fc506e8ac85b100158ded74214257f88bdc694db
woong <workplayhard1@naver.com>
2023-07-31 01:24:03 +0900

    feat: Message send 버튼 초기구현

--- MessageKitPractice/MessageKitPractice/ChatViewController.swift
+++ MessageKitPractice/MessageKitPractice/ChatViewController.swift
@@ -65,8 +65,45 @@ extension ChatExamViewController: MessagesDisplayDelegate {
 
 extension ChatExamViewController: InputBarAccessoryViewDelegate {
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
-        print(text)
+        processInputBar(inputBar)
     }
+    
+    private func processInputBar(_ inputBar: InputBarAccessoryView) {
+        let components = inputBar.inputTextView.components
+        
+        inputBar.inputTextView.text = String()
+        inputBar.invalidatePlugins() // 이유알아보기
+        inputBar.sendButton.startAnimating()
+        inputBar.inputTextView.placeholder = "Sending...!"
+        
+        DispatchQueue.global().async {
+            //fake
+            sleep(1)
+            
+            DispatchQueue.main.async { [weak self] in
+                inputBar.sendButton.stopAnimating()
+                inputBar.inputTextView.placeholder = "Aa"
+                self?.insertMessages(components)
+                self?.messagesCollectionView.scrollToLastItem()
+            }
+        }
+    }
+    
+    private func insertMessages(_ data: [Any]) {
+      for component in data {
+        let user = currentSender
+        if let str = component as? String {
+            let message = Message(sender: user, messageId: UUID().uuidString, sentDate: Date(), kind: .text(str))
+            insertMessage(message)
+        }
+      }
+    }
+    
+    private func insertMessage(_ message: Message) {
+        messages.append(message)
+        messagesCollectionView.reloadData()
+    }
+    
 }
 
 // MARK: TestData Injection
