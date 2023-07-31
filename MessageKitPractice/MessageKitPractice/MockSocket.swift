ac3aea8e2b680cf718bde212165001931ca61f36
woong <workplayhard1@naver.com>
2023-07-31 16:34:52 +0900

    chore: SampleData 일부수정

--- MessageKitPractice/MessageKitPractice/MockSocket.swift
+++ MessageKitPractice/MessageKitPractice/MockSocket.swift
@@ -58,7 +58,7 @@ class MockSocket {
                     messageId: UUID().uuidString,
                     date: Date()),
             
-            Message(text: "두번째 텍서터",
+            Message(text: "텍스트 형태의 메시지입니다.",
                     sender: sender,
                     messageId: UUID().uuidString,
                     date: Date()),
@@ -83,12 +83,22 @@ class MockSocket {
                     messageId: UUID().uuidString,
                     date: Date()),
             
-            Message(image: UIImage(named: "screenshot1")!,
+            Message(text: "장문의 텍스트를 입력하고있습니다. abcdEFGKLAJKLFDJLFDJSLKFJKLSJFKLSFJSLKDFJSDLFSDJFLKSJDFLKSJFLKSDFJSDLKFJSDKLFJSDLFKJSDFLKSDJFLKSDFJLSDKJFLDKSFJSDLKFJSDLFKSJDFLKJSDLFKJDKLFJSDLKFJSDLFKJSDLFKJSDFLKSDJFLKSDJFKLSDFJSDLKFJSDLFKJSDLFKJSDFLKSDJ",
                     sender: sender,
                     messageId: UUID().uuidString,
                     date: Date()),
             
-            Message(image: UIImage(named: "screenshot1")!,
+            Message(image: UIImage(named: "Steve-Jobs")!,
+                    sender: sender,
+                    messageId: UUID().uuidString,
+                    date: Date()),
+            
+            Message(image: UIImage(named: "Steven-Deutsch")!,
+                    sender: sender,
+                    messageId: UUID().uuidString,
+                    date: Date()),
+            
+            Message(text: "메시지킷은 텍스트, 어트리뷰트텍스트, 이모지, 사진, 비디오, 지도, 오디오, 연락처 양식의 메시지를 제공하는 것 같아요.,",
                     sender: sender,
                     messageId: UUID().uuidString,
                     date: Date())
@@ -96,7 +106,17 @@ class MockSocket {
         
         return sampleMessages.randomElement()!
     }
-    
 }
 
 
+/*
+ text(String)- 속성 없이 일반 텍스트 메시지를 표시하려는 경우 이 경우를 사용하십시오.
+ attributedText(NSAttributedString)- 속성이 있는 텍스트 메시지를 표시하려는 경우 이 경우를 사용하십시오.
+ attributedText : 문자 메시지에 사용하는 것을 권장합니다 .
+ emoji(String)- 이모티콘만 포함된 메시지를 표시하려면 이 케이스를 사용하십시오.
+ photo(MediaItem)- 사진 메시지를 표시하려면 이 케이스를 사용하십시오.
+ video(MediaItem)- 영상 메시지를 표시할 때 사용하세요.
+ location(LocationItem)- 이 케이스를 사용하여 위치 메시지를 표시합니다.
+ audio(AudioItem)- 오디오 메시지를 표시하려면 이 케이스를 사용하십시오.
+ contact(ContactItem)- 연락처 메시지를 표시하려면 이 케이스를 사용하십시오.
+ */
